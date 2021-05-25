6027a0b4ced5e2b4c633d958f98279d273b27d77
joelox87 <j.aponte87@gmail.com>
2021-05-24 22:39:32 -0500

    updates

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,28 @@
+# Import Dependencies
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Flask setup
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+
+    mars_page = mongo.db.mars_page.find_one()
+    return render_template("index.html", mars=mars_page)
+
+@app.route("/scrape")
+def scrape():
+  
+    mars_page = mongo.db.mars_page
+    mars_data = scrape_mars.scrape()
+    mars_page.update({}, mars_data, upsert=True)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
